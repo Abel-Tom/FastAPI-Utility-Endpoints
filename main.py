7d88a24fcea6ed9446da132b6ecf39c6c3dbf39a
Abel Tom <icecakeinc@gmail.com>
2025-05-03 20:00:10 +0530

    added cors

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import time
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 from api.email import email_router
 
 app = FastAPI(title="FastAPI Demo App")
+
+origins = [
+    "http://localhost:3000",  # React dev server
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "https://abel-tom.vercel.app",  # Add your production frontend domain here
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all (less secure)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 app.include_router(email_router)
